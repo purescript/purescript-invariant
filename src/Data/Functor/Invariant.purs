3e48ce21e7864b16bc81233c10ecf4e69441fdb4
Gary Burgess <gary.burgess@gmail.com>
2015-04-12 15:20:23 +0100

    Rename invmap to imap

--- src/Data/Functor/Invariant.purs
+++ src/Data/Functor/Invariant.purs
@@ -6,17 +6,17 @@ module Data.Functor.Invariant where
 -- |
 -- | An `Invariant` instance should satisfy the following laws:
 -- |
--- | - Identity: `invmap id id = id`
--- | - Composition: `invmap g1 g2 <<< invmap f1 f2 = invmap (g1 <<< f1) (f2 <<< g2)`
+-- | - Identity: `imap id id = id`
+-- | - Composition: `imap g1 g2 <<< imap f1 f2 = imap (g1 <<< f1) (f2 <<< g2)`
 -- |
 class Invariant f where
-  invmap :: forall a b. (a -> b) -> (b -> a) -> f a -> f b
+  imap :: forall a b. (a -> b) -> (b -> a) -> f a -> f b
 
 -- | As all `Functor`s are also trivially `Invariant`, this function can be
--- | used as the `invmap` implementation for all `Invariant` instances for
+-- | used as the `imap` implementation for all `Invariant` instances for
 -- | `Functors`.
-invmapF :: forall f a b. (Functor f) => (a -> b) -> (b -> a) -> f a -> f b
-invmapF = const <<< (<$>)
+imapF :: forall f a b. (Functor f) => (a -> b) -> (b -> a) -> f a -> f b
+imapF = const <<< (<$>)
 
 instance invariantArr :: Invariant ((->) a) where
-  invmap = invmapF
+  imap = imapF
